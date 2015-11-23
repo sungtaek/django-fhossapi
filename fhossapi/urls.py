f63ae19c8ebcf8d0a961ee0c5a734f3426f5ec00
sungtaek <leesungtaek@gmail.com>
2015-11-23 15:20:25 +0900

    ...

--- fhossapi/urls.py
+++ fhossapi/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^auth-token/$', views.AuthToken.as_view()),
-    url(r'^user/$', views.UserList.as_view()),
-    url(r'^user/_add$', views.UserAdd.as_view()),
-    url(r'^user/(?P<identity>[:0-9A-Za-z_\-\.@]+)/$', views.User.as_view()),
+    url(r'^user/$', views.UserListView.as_view()),
+    url(r'^user/_add$', views.UserAddView.as_view()),
+    url(r'^user/(?P<identity>[:0-9A-Za-z_\-\.@]+)/$', views.UserView.as_view()),
     url(r'^service/$', views.ServiceList.as_view()),
     url(r'^service/_add$', views.ServiceAdd.as_view()),
     url(r'^service/(?P<identity>[0-9]+)/$', views.Service.as_view()),
