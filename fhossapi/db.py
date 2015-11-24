045f46e7e7410a41d79527cc14cd9429e886f4d8
sungtaek <leesungtaek@gmail.com>
2015-11-24 21:43:58 +0900

    change model

--- fhossapi/db.py
+++ fhossapi/db.py
@@ -28,3 +28,181 @@ class Database(object):
 	def fetch_one(self):
 		return self.cursor.fetchone()
 
+
+class Field(object):
+	STRING = 0
+	INTEGER = 1
+
+	def __init__(self, db_column, type, nullable=True, default=None, dictionable=True):
+		self.db_column = db_column
+		self.type = type
+		self.nullable = nullable
+		self.default = default
+		self.dictionable = dictionable
+
+class BaseModel(object):
+	db = Database()
+	
+	'''
+	Must override below variables
+	table : table name
+	fields : table columns info list (column_key, db_column_name, default_value, dict_yn)
+	
+	ex) mysql> desc user_tbl;
+	+------------------------+--------------+------+-----+--------------+----------------+
+	| Field                  | Type         | Null | Key | Default      | Extra          |
+	+------------------------+--------------+------+-----+--------------+----------------+
+	| id                     | int(11)      | NO   | PRI | NULL         | auto_increment |
+	| name                   | varchar(255) | NO   | MUL |              |                |
+	| address                | varchar(255) | YES  |     | NULL         |                |
+	+------------------------+--------------+------+-----+--------------+----------------+
+
+	table = 'user_tbl'
+	identity = Field('id', Field.INTEGER, nullable=False, default=-1, dictionable=False)
+	user_name = Field('name', Field.STRING, nullable=False)
+	address = Field('address', Field.STRING)
+	'''
+	table = None
+
+	def __init__(self, **kwargs):
+		for key, val in self._get_all_fields().iteritems():
+			if kwargs.has_key(key):
+				setattr(self, key, kwargs[key])
+			elif val.default is not None:
+				setattr(self, key, val.default)
+			elif val.nullable is False:
+				raise ValueError("%s must be setted" % (key))
+			else:
+				setattr(self, key, None)
+			
+	@classmethod
+	def _get_field(cls, key):
+		if key in cls.__dict__.keys():
+			val = cls.__dict__[key]
+			if isinstance(val, Field):
+				return val
+		return None
+	
+	@classmethod
+	def _get_all_fields(cls):
+		fields = {}
+		for key in cls.__dict__.keys():
+			val = cls.__dict__[key]
+			if isinstance(val, Field):
+				fields[key] = val
+		return fields
+			
+	@classmethod
+	def create_by_row(cls, row):
+		args = {}
+		for key, val in cls._get_all_fields().iteritems():
+			sel_name = cls.sel_name(key)
+			if row.has_key(sel_name):
+				args[key] = row[sel_name]
+			elif row.has_key(val.db_column):
+				args[key] = row[val.db_column]
+		return cls(**args)
+	
+	@classmethod
+	def col_name(cls, key):
+		val = cls._get_field(key)
+		if val:
+			return cls.table + '.' + val.db_column
+		return None
+	
+	@classmethod
+	def sel_name(cls, key):
+		val = cls._get_field(key)
+		if val:
+			return cls.table + '_' + val.db_column
+		return None
+
+	@classmethod
+	def col_as_sel_names(cls):
+		names = []
+		for key in cls._get_all_fields().iterkeys():
+			names.append('%s as %s' % (cls.col_name(key), cls.sel_name(key)))
+		return names
+
+	@classmethod
+	def get(cls, **kwargs):
+		obj = None
+		q_names = []
+		q_values = []
+
+		for name, value in kwargs.items():
+			if cls._get_field(name):
+				q_names.append('%s=%%s' % (name))
+				q_values.append(value)
+			else:
+				raise ValueError("unknown field_key[%s]" % (name))
+
+		q = 'select %s from %s' % (','.join(cls.col_as_sel_names()), cls.table)
+		if len(q_names) > 0:
+			q = q + ' where ' + ' and '.join(q_names)
+		
+		logger.debug('query -> %s' % (q))
+		for value in q_values:
+			logger.debug('values -> {}'.format(value))
+		row_num = cls.db.execute(q, tuple(q_values))
+		logger.debug('result <- %d rows' % (row_num))
+		if row_num > 0:
+			row = cls.db.fetch_one()
+			obj = cls._init_by_row(row)
+		
+		return obj
+	
+	@classmethod
+	def search(cls, **kwargs):
+		objs = []
+		q_names = []
+		q_values = []
+		
+		for name, value in kwargs.items():
+			field_val = cls._get_field(name)
+			if field_val:
+				if field_val.type == Field.STRING:
+					q_name.append('%s like %%s' % (name))
+					q_values.append('%' + value + '%')
+				else:
+					q_name.append('%s=%%s' % (name))
+					q_values.append(value)
+			else:
+				raise ValueError("unknown field_key[%s]" % (name))
+
+		q = 'select %s from %s' % (','.join(cls.col_as_sel_names()), cls.table)
+		if len(q_names) > 0:
+			q = q + ' where ' + ' and '.join(q_names)
+		
+		logger.debug('query -> %s' % (q))
+		for value in q_values:
+			logger.debug('values -> {}'.format(value))
+		row_num = cls.db.execute(q, tuple(q_values))
+		logger.debug('result <- %d rows' % (row_num))
+		if row_num > 0:
+			row = cls.db.fetch_one()
+			while row:
+				objs.append(cls._init_by_row(row))
+				row = cls.db.fetch_one()
+		
+		return objs
+	
+	def save(self):
+		'''
+		must override this function
+		'''
+		return True
+
+	def delete(self):
+		'''
+		must override this function
+		'''
+		return True
+	
+	def dict(self):
+		value = {}
+		for key, val in self._get_all_fields().iteritems():
+			if val.dictionable and self.__dict__.has_key(key):
+				value[key] = self.__dict__[key]
+				
+				
--- fhossapi/models.py
+++ fhossapi/models.py
@@ -1,134 +1,30 @@
 from django.db import models
-from .db import Database
+from .db import BaseModel, Field
 import logging
 
 # Create your models here.
 
 logger = logging.getLogger('fhossapi')
 
-class BaseModel(object):
-	db = Database()
-	table = None
-
-	@classmethod
-	def get(cls, **kwargs):
-		obj = None
-
-		q = 'select * from %s' % (cls.table)
-		q_args = []
-		first = True
-		for name, value in kwargs.items():
-			if first:
-				q = '%s where' % (q)
-			else :
-				q = '%s and' % (q)
-			first = False
-			
-			q = '%s %s=%%s' % (q, name)
-			q_args.append(value)
-		
-		logger.debug('query -> %s' % (q))
-		for value in q_args:
-			logger.debug('args -> {}'.format(value))
-		row_num = cls.db.execute(q, tuple(q_args))
-		logger.debug('result <- %d rows' % (row_num))
-		if row_num > 0:
-			row = cls.db.fetch_one()
-			obj = cls._init_by_row(row)
-		
-		return obj
-	
-	@classmethod
-	def search(cls, **kwargs):
-		objs = []
-
-		q = 'select * from %s' % (cls.table)
-		q_args = []
-		first = True
-		for name, value in kwargs.items():
-			if first:
-				q = '%s where' % (q)
-			else:
-				q = '%s and' % (q)
-			first = False
-			
-			if type(value) is str or type(value) is unicode:
-				q = '%s %s like %%s' % (q, name)
-				value = '%' + value + '%'
-			else:
-				q = '%s %s = %%s' % (q, name)
-			q_args.append(value)
-		
-		logger.debug('query -> %s' % (q))
-		for value in q_args:
-			logger.debug('args -> {}'.format(value))
-		row_num = cls.db.execute(q, tuple(q_args))
-		logger.debug('result <- %d rows' % (row_num))
-		if row_num > 0:
-			row = cls.db.fetch_one()
-			while row:
-				objs.append(cls._init_by_row(row))
-				row = cls.db.fetch_one()
-		
-		return objs
-	
-	@classmethod
-	def _init_by_row(cls, row):
-		'''
-		must override this function
-		'''
-		return cls()
-
-	def save(self):
-		'''
-		must override this function
-		'''
-		return True
-
-	def delete(self):
-		'''
-		must override this function
-		'''
-		return True
-
-	
 class User(BaseModel):
 	table = 'imsu'
-	
-	def __init__(self, name, id=-1, capa_set=-1, pref_scscf='', scscf_name='', diameter_name=''):
-		self.id = id
-		self.name = name
-		self.capa_set = capa_set
-		self.pref_scscf = pref_scscf
-		self.scscf_name = scscf_name
-		self.diameter_name = diameter_name
-		self.impi = None
-		self.impu = None
-		
-	def __dict__(self):
-		val = {}
-		val['name'] = self.name
-		val['capa_set'] = self.capa_set
-		val['pref_scscf'] = self.pref_scscf
-		val['scscf_name'] = self.scscf_name
-		val['diameter_name'] = self.diameter_name
+	id			= Field('id', Field.INTEGER, nullable=False, default=-1, dictionable=False)
+	name		= Field('name', Field.STRING, nullable=False)
+	scscf_name	= Field('scscf_name', Field.STRING)
+	diameter_name= Field('diameter_name', Field.STRING)
+	capa_set	= Field('id_capabilities_set', Field.INTEGER)
+	pref_scscf	= Field('id_preferred_scscf_set', Field.INTEGER)
+
+	def dict(self):
+		value = User.dict()
 		if self.impi:
-			val['impi'] = self.impi.__dict__()
+			value['impi'] = self.impi.dict()
 		if self.impu:
-			val['impu'] = self.impu.__dict__()
-		return val
+			value['impu'] = self.impu.dict()
+		return value
 
 	@classmethod
-	def _init_by_row(cls, row):
-		return cls(name = row['name']
-				, capa_set = row['id_capabilities_set']
-				, pref_scscf = row['id_preferred_scscf_set']
-				, scscf_name = row['imsu.scscf_name']
-				, diameter_name= row['imsu.diameter_name'])
-
-			
-	@classmethod
-	def get_by_impi(cls, **kwargs):
+	def get_with_impi_impu(cls, **kwargs):
 		user = None
 		impi = Impi.get(**kwargs)
 		if impi and impi.id_imsu >= 0:
@@ -138,31 +34,7 @@ class User(BaseModel):
 		return user
 	
 	@classmethod
-	def get_by_impu(cls, **kwargs):
-		user = None
-		impu = Impu.get(**kwargs)
-		if impu:
-			impi = Impi.get_by_impu(impu.id)
-			if impi and impi.id_imsu>= 0:
-				user = User.get(id=impi.id_imsu)
-				user.impi = impi
-				user.impu = impu
-		return user
-
-	@classmethod
-	def search_by_impi(cls, **kwargs):
-		users = []
-		impis = Impi.search(**kwargs)
-		for impi in impis:
-			if impi.id_imsu >= 0:
-				user = User.get(id=impi.id_imsu)
-				if user:
-					user.impi = impi
-					users.append(user)
-		return users
-	
-	@classmethod
-	def search_by_impu(cls, **kwargs):
+	def search_with_impi_impu(cls, **kwargs):
 		users = []
 		impus = Impu.search(**kwargs)
 		for impu in impus:
@@ -176,98 +48,29 @@ class User(BaseModel):
 
 class Impi(BaseModel):
 	table = 'impi'
-	
-	def __init__(self, identity, secret_key, id=-1
-				, id_imsu = -1, avail_auth = 129, def_auth = 128):
-		self.id = id
-		self.id_imsu = id_imsu
-		self.identity = identity
-		self.secret_key = secret_key
-		self.avail_auth = avail_auth
-		self.def_auth = def_auth
-		self.amf = '0000'
-		self.op = '00000000000000000000000000000000'
-		self.sgn = '000000000000'
-		self.early_ims_ip = ''
-		self.dsl_line_identifier = ''
-
-	def __dict__(self):
-		val = {}
-		val['identity'] = self.identity
-		val['secret_key'] = self.secret_key
-		val['avail_auth'] = self.avail_auth
-		val['def_auth'] = self.def_auth
-		val['amf'] = self.amf
-		val['op'] = self.op
-		val['sgn'] = self.sgn
-		val['early_ims_ip'] = self.early_ims_ip
-		val['dsl_line_identifier'] = self.dsl_line_identifier
-		return val
-		
-	@classmethod	
-	def _init_by_row(cls, row):
-		return cls(id = row['id']
-				, id_imsu = row['id_imsu']
-				, identity = row['identity']
-				, secret_key = row['k']
-				, avail_auth = row['auth_scheme']
-				, def_auth = row['default_auth_scheme'])
-		
-	@classmethod
-	def get_by_impu(cls, impu_id):
-		impi = None
-		q = 'select id_impi from impi_impu where id_impu=%%s'
-		row_num = cls.db.execute(q, (impu_id))
-		if row_num > 0:
-			row = cls.db.fetch_one()
-			impi = Impi.get(id=row['id_impi'])
-		return impi
+	id			= Field('id', Field.INTEGER, nullable=False, default=-1, dictionable=False)
+	id_imsu		= Field('id_imsu', Field.INTEGER, default=-1, dictionable=False)
+	identity	= Field('identity', Field.STRING, nullable=False)
+	secret_key	= Field('k', Field.STRING, nullable=False)
+	avail_auth	= Field('auth_scheme', Field.INTEGER, nullable=False, default=129)
+	def_auth	= Field('default_auth_scheme', Field.INTEGER, nullable=False, default=128)
+	amf			= Field('amf', Field.STRING, nullable=False, default='0000')
+	op			= Field('op', Field.STRING, nullable=False, default='00000000000000000000000000000000')
+	sgn			= Field('sgn', Field.STRING, nullable=False, default='000000000000')
+	early_ims_ip= Field('ip', Field.STRING, nullable=False, default='')
+	dsl_line_id	= Field('line_identifier', Field.STRING, nullable=False, default='')
 	
 class Impu(BaseModel):
 	table = 'impu'
-	
-	def __init__(self, identity, service_profile, id=-1
-				, user_status=0, impu_type=0, barring=False
-				, charging_info_set=-1, can_register=True
-				, wildcard_psi='', psi_activation=False
-				, display_name='', visited_networks=[]):
-		self.id = id
-		self.identity = identity
-		self.service_profile = service_profile
-		self.user_status = user_status
-		self.impu_type = impu_type 
-		self.barring = barring
-		self.charging_info_set = charging_info_set
-		self.can_register = can_register
-		self.wildcard_psi = wildcard_psi
-		self.psi_activation = psi_activation
-		self.display_name = display_name
-		self.visited_networks = visited_networks
-		
-	def __dict__(self):
-		val = {}
-		val['identity'] = self.identity
-		val['service_profile'] = self.service_profile
-		val['user_status'] = self.user_status
-		val['impu_type'] = self.impu_type
-		val['barring'] = self.barring
-		val['charging_info_set'] = self.charging_info_set
-		val['wildcard_psi'] = self.wildcard_psi
-		val['psi_acrivation'] = self.psi_activation
-		val['display_name'] = self.display_name
-		val['visited_networks'] = self.visited_networks
-		return val
-		
-	@classmethod
-	def _init_by_row(cls, row):
-		return cls(id = row['id']
-				, identity = row['identity']
-				, service_profile = row['id_sp']
-				, user_status = row['user_state']
-				, impu_type = row['type']
-				, barring = (row['barring'] == 1)
-				, charging_info_set = row['id_charging_info']
-				, can_register = (row['can_register'] == 1)
-				, wildcard_psi = row['wildcard_psi']
-				, psi_activation = (row['psi_activation'] == 1)
-				, display_name = row['display_name'])
+	id			= Field('id', Field.INTEGER, nullable=False, default=-1, dictionable=False)
+	identity	= Field('identity', Field.STRING, nullable=False)
+	impu_type	= Field('type', Field.INTEGER, nullable=False, default=0)
+	barring		= Field('barring', Field.INTEGER, nullable=False, default=0)
+	user_status	= Field('user_state', Field.INTEGER, nullable=False, default=0)
+	service_profile	= Field('id_sp', Field.INTEGER, nullable=False)
+	id_implicit_set	= Field('id_implict_set', Field.INTEGER, nullable=False, default=-1, dictionable=False)
+	charging_info_set= Field('id_charging_info', Field.INTEGER, default=-1)
+	wildcard_psi	= Field('wildcard_psi', Field.STRING, nullable=False, default='')
+	display_name	= Field('display_name', Field.STRING, nullable=False, default='')
+	psi_activation	= Field('psi_activation', Field.INTEGER, nullable=False, default=0)
+	can_register	= Field('can_register', Field.INTEGER, nullable=False, default=0)
