74bed463b03cd9e0d630ad702622bbace23af70a
sungtaek <leesungtaek@gmail.com>
2015-11-26 15:44:58 +0900

    ...

--- fhossapi/views.py
+++ fhossapi/views.py
@@ -147,7 +147,7 @@ class UserDetailView(APIView):
 	# permission_classes = (IsAuthenticated,)
 
 	def get(self, request, name):
-		user = Imsu.objects.select_related('impis__impus').get(name=name).values()
+		user = Imsu.objects.get(name=name).values()
 		return Response(user)
 
 	def put(self, request, name):
